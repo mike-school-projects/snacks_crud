b803ad51fec81c42f81010a9c99191528cde43c6
Mike Shen <michael.m.shen@gmail.com>
2023-04-26 21:14:49 +0000

    added files

---
+++ README.md
@@ -0,0 +1,27 @@
+# LAB - Class 28
+
+## Project: Django CRUD
+
+## Author: Mike Shen
+
+## Links and Resources
+N/A
+
+##  Setup
+per requirements.txt
+
+Need to set up .env file in snacks_crud_project folder for Django secret key.  See .env.sample
+
+# How to initialize/run your application (where applicable)
+
+From root folder, run:
+
+python manage.py runserver
+
+## How to use your library (where applicable)
+
+## Tests
+
+From root folder, run:
+
+python manage.py test
---
+++ snacks/views.py
@@ -0,0 +1,27 @@
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Snack
+from django.urls import reverse_lazy
+
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+    context_object_name = 'snacks'
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
+
+class SnackCreateView(CreateView):
+    template_name = 'snack_create.html'
+    model = Snack
+    fields = ['name', 'purchaser', 'description']
+
+class SnackUpdateView(UpdateView):
+    template_name = 'snack_update.html'
+    model = Snack
+    fields = ['name', 'purchaser', 'description']
+
+class SnackDeleteView(DeleteView):
+    template_name = 'snack_delete.html'
+    model = Snack
+    success_url = reverse_lazy('snack_list')
